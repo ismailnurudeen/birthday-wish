8204591288cbf721d245ff0c7f2ef2e03382bb88
Ismail Nurudeen <ibrightstar247@gmail.com>
2018-09-26 06:13:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const wishView = document.querySelector(".live-wish p");
+let wish_html='Happy birthday to you <b>Winfred Aideyan Iwinosa.</b><br/>I wish you <span class="w1">long life</span>,<span class="w2">prosperity</span> and <span class="w3">happiness.</span><br/>Thank you for your words of encouragement and advice.<br/>I am glad to have come across you in my life.<br/>Enjoy your day and i wish you bug free codes.<br/><br/><span class="from"> - From someone who cares</span>'
+let wish="Happy birthday to you Winfred Aideyan Iwinosa. I wish you long life,prosperity and happiness.Thank you for your words of encouragement and advice.I am glad to have come across you in my life.Enjoy your day and i wish you bug free codes. - From someone who cares";
+var pos = 0;
+var speed = 130;
+function write(text) {
+    if (pos < text.length) {
+        wishView.innerHTML += (text.charAt(pos));
+        pos++;
+    }
+    if(pos == text.length){
+        wishView.innerHTML=wish_html;
+        for(var i=0;i<=5;i++){
+         cornify_add();
+    }
+   }
+}
+function playWish(words) {
+    writeNote = setInterval(() => {
+        write(words);
+    }, speed);
+}
+playWish(wish);
