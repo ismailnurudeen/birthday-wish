eed52a9e9ab14170a553e3c2518f3300d97d5d32
Ismail Nurudeen <ibrightstar247@gmail.com>
2018-09-26 07:17:00 +0100

    add cornify js

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ const wishView = document.querySelector(".live-wish p");
 let wish_html='Happy birthday to youWinfred Aideyan Iwinosa.<br/>I wish you <span class="w1">long life</span>,<span class="w2">prosperity</span> and <span class="w3">happiness.</span><br/>Thank you for your words of encouragement and advice.<br/>I am glad to have come across you in my life.<br/>Enjoy your day and i wish you bug free codes.<br/><br/><span class="from"> - From someone who cares</span>'
 let wish="Happy birthday to you Winfred Aideyan Iwinosa. I wish you long life,prosperity and happiness.Thank you for your words of encouragement and advice.I am glad to have come across you in my life.Enjoy your day and i wish you bug free codes. - From someone who cares";
 var pos = 0;
-var speed = 130;
+var speed = 1;
+var writeNote;
 function write(text) {
     if (pos < text.length) {
         wishView.innerHTML += (text.charAt(pos));
         pos++;
     }
     if(pos == text.length){
+        clearInterval(writeNote);
         wishView.innerHTML=wish_html;
         for(var i=0;i<=5;i++){
-         cornify_add();
-    }
+            cornify_add();
+        }
    }
 }
 function playWish(words) {
@@ -21,3 +23,4 @@ function playWish(words) {
     }, speed);
 }
 playWish(wish);
+        
